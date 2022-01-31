275e3299899087eaf9af6c504f7172f8483ab2a8
Maksym Hontar <maksym@springsoftware.io>
2022-01-31 21:56:12 +0200

    fix binance streaming available amount

--- xchange-ftx/src/main/java/org/knowm/xchange/ftx/service/FtxAccountService.java
+++ xchange-ftx/src/main/java/org/knowm/xchange/ftx/service/FtxAccountService.java
@@ -2,7 +2,6 @@ package org.knowm.xchange.ftx.service;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.dto.account.AccountInfo;
-import org.knowm.xchange.dto.account.OpenPosition;
 import org.knowm.xchange.ftx.FtxAdapters;
 import org.knowm.xchange.ftx.dto.FtxResponse;
 import org.knowm.xchange.ftx.dto.account.FtxAccountDto;
@@ -11,7 +10,6 @@ import org.knowm.xchange.service.account.params.AccountLeverageParams;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.List;
 
 public class FtxAccountService extends FtxAccountServiceRaw implements AccountService {
 
