47b30352053c274e5ecb8dc9440bde046ff9f6fb
vasylp3 <82892107+vasylp3@users.noreply.github.com>
2022-04-19 12:23:54 +0300

    Added empty limiter test

--- xchange-core/src/test/java/org/knowm/xchange/client/PlaceOrderLimiterTest.java
+++ xchange-core/src/test/java/org/knowm/xchange/client/PlaceOrderLimiterTest.java
@@ -73,6 +73,20 @@ public class PlaceOrderLimiterTest {
         }
     }
 
+    @Test
+    public void testEmptyLimiter() throws Exception {
+        executor = new PlaceOrderLimiter(0, 100, 10_000L);
+
+        long t0 = System.currentTimeMillis();
+        executor.executePlace(() -> emptyPlaceOrder(null));
+        executor.executePlace(() -> emptyPlaceOrder(null));
+        executor.executePlace(() -> emptyPlaceOrder(null));
+        executor.executePlace(() -> emptyPlaceOrder(null));
+        long t1 = System.currentTimeMillis();
+
+        assertThat(t1 - t0 < 100).isTrue();
+    }
+
     private String emptyPlaceOrder(IOException exception) throws IOException {
         if (exception != null) throw exception;
         return String.valueOf(System.currentTimeMillis());
