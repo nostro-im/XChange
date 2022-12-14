370283f80c7a82689e5f46656eac9c101b20d31f
EzBar <ezerba@gmail.com>
2022-12-14 19:34:48 +0200

    [BinanceFutures] Update Authenticated interface
    BinanceExchange services C'tor expect BinanceAuthenticated object.
    BinanceAuthenticated extends Binance hence calls the SPOT market-data-service methods.
    
    BinanceFuturesAuthenticated extends BinanceAuthenticated, so that it can be accepted by the services.
    Trying to have it extend a new BinanceFutures interface that implements the FUTURES API for market-data won't work, unless we fully duplicate the exchange.
    
    Our solution is to override ALL market-data methods with the FUTURES URL, within BinanceFuturesAuthenticated

--- xchange-binance/src/main/java/org/knowm/xchange/binance/futures/BinanceFuturesAuthenticated.java
+++ xchange-binance/src/main/java/org/knowm/xchange/binance/futures/BinanceFuturesAuthenticated.java
@@ -1,32 +1,228 @@
 package org.knowm.xchange.binance.futures;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
 import org.knowm.xchange.binance.BinanceAuthenticated;
 import org.knowm.xchange.binance.dto.BinanceException;
 import org.knowm.xchange.binance.dto.account.BinanceMarginPositionSide;
 import org.knowm.xchange.binance.dto.account.BinanceMarginType;
 import org.knowm.xchange.binance.dto.marketdata.BinanceAggTrades;
 import org.knowm.xchange.binance.dto.marketdata.BinanceOrderbook;
+import org.knowm.xchange.binance.dto.marketdata.BinancePrice;
+import org.knowm.xchange.binance.dto.marketdata.BinancePriceQuantity;
 import org.knowm.xchange.binance.dto.marketdata.BinanceTicker24h;
-import org.knowm.xchange.binance.dto.trade.*;
-import org.knowm.xchange.binance.futures.dto.account.*;
+import org.knowm.xchange.binance.dto.meta.BinanceTime;
+import org.knowm.xchange.binance.dto.meta.exchangeinfo.BinanceExchangeInfo;
+import org.knowm.xchange.binance.dto.trade.BinanceCancelledOrder;
+import org.knowm.xchange.binance.dto.trade.BinanceListenKey;
+import org.knowm.xchange.binance.dto.trade.OrderSide;
+import org.knowm.xchange.binance.dto.trade.TimeInForce;
+import org.knowm.xchange.binance.futures.dto.account.BinanceFuturesAccountInformation;
+import org.knowm.xchange.binance.futures.dto.account.BinanceFuturesIncomeHistoryRecord;
+import org.knowm.xchange.binance.futures.dto.account.BinanceFuturesInitialLeverage;
+import org.knowm.xchange.binance.futures.dto.account.BinanceFuturesLeverageBrackets;
+import org.knowm.xchange.binance.futures.dto.account.BinanceFuturesPositionInformation;
+import org.knowm.xchange.binance.futures.dto.account.BinanceUserCommissionRate;
 import org.knowm.xchange.binance.futures.dto.meta.BinanceFuturesExchangeInfo;
-import org.knowm.xchange.binance.futures.dto.trade.*;
+import org.knowm.xchange.binance.futures.dto.trade.BinanceFuturesOrder;
+import org.knowm.xchange.binance.futures.dto.trade.BinanceFuturesOrderType;
+import org.knowm.xchange.binance.futures.dto.trade.BinanceFuturesTrade;
+import org.knowm.xchange.binance.futures.dto.trade.NewOrderRespType;
+import org.knowm.xchange.binance.futures.dto.trade.PositionSide;
+import org.knowm.xchange.binance.futures.dto.trade.WorkingType;
+import org.knowm.xchange.exceptions.NotAvailableFromExchangeException;
+
 import si.mazi.rescu.ParamsDigest;
 import si.mazi.rescu.SynchronizedValueFactory;
-
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
 
 @Path("")
 @Produces(MediaType.APPLICATION_JSON)
 public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
 
-    @GET
-    @Path("fapi/v2/account")
+	// -------------------- Override Binance interface methods with Futures Url -------------------
+
+	/**
+	  * Test connectivity to the Rest API.
+	  *
+	  * @return
+	  * @throws IOException
+	  */
+	@Override
+	@GET
+	@Path("fapi/v1/ping")
+	Object ping() throws IOException;
+
+	/**
+	 * Test connectivity to the Rest API and get the current server time.
+	 *
+	 * @return
+	 * @throws IOException
+	 */
+	@Override
+	@GET
+	@Path("fapi/v1/time")
+	BinanceTime time() throws IOException;
+
+	/**
+	 * Current exchange trading rules and symbol information.
+	 *
+	 * @return
+	 * @throws IOException
+	 */
+	default BinanceExchangeInfo exchangeInfo() throws IOException {
+		throw new NotAvailableFromExchangeException();
+	}
+
+	/**
+	 * @param symbol
+	 * @param limit optional, default 100 max 5000. Valid limits: [5, 10, 20, 50, 100, 500, 1000,
+	 *     5000]
+	 * @return
+	 * @throws IOException
+	 * @throws BinanceException
+	 */
+	@Override
+	@GET
+	@Path("fapi/v1/depth")
+	BinanceOrderbook depth(@QueryParam("symbol") String symbol, @QueryParam("limit") Integer limit)
+			throws IOException, BinanceException;
+	
+	/**
+	 * Get compressed, aggregate trades. Trades that fill at the time, from the same order, with the
+	 * same price will have the quantity aggregated.<br>
+	 * If both startTime and endTime are sent, limit should not be sent AND the distance between
+	 * startTime and endTime must be less than 24 hours.<br>
+	 * If frondId, startTime, and endTime are not sent, the most recent aggregate trades will be
+	 * returned.
+	 *
+	 * @param symbol
+	 * @param fromId optional, ID to get aggregate trades from INCLUSIVE.
+	 * @param startTime optional, Timestamp in ms to get aggregate trades from INCLUSIVE.
+	 * @param endTime optional, Timestamp in ms to get aggregate trades until INCLUSIVE.
+	 * @param limit optional, Default 500; max 500.
+	 * @return
+	 * @throws IOException
+	 * @throws BinanceException
+	 */
+	@Override
+	@GET
+	@Path("fapi/v1/aggTrades")
+	List<BinanceAggTrades> aggTrades(
+	    @QueryParam("symbol") String symbol,
+	    @QueryParam("fromId") Long fromId,
+	    @QueryParam("startTime") Long startTime,
+	    @QueryParam("endTime") Long endTime,
+	    @QueryParam("limit") Integer limit)
+	    throws IOException, BinanceException;
+
+	/**
+	 * Kline/candlestick bars for a symbol. Klines are uniquely identified by their open time.<br>
+	 * If startTime and endTime are not sent, the most recent klines are returned.
+	 *
+	 * @param symbol
+	 * @param interval
+	 * @param limit optional, default 500; max 500.
+	 * @param startTime optional
+	 * @param endTime optional
+	 * @return
+	 * @throws IOException
+	 * @throws BinanceException
+	 */
+	@Override
+	@GET
+	@Path("fapi/v1/klines")
+	List<Object[]> klines(
+	    @QueryParam("symbol") String symbol,
+	    @QueryParam("interval") String interval,
+	    @QueryParam("limit") Integer limit,
+	    @QueryParam("startTime") Long startTime,
+	    @QueryParam("endTime") Long endTime)
+	    throws IOException, BinanceException;
+
+	/**
+	 * 24 hour price change statistics for all symbols. - bee careful this api call have a big
+	 * weight, only about 4 call per minute can be without ban.
+	 *
+	 * @return
+	 * @throws IOException
+	 * @throws BinanceException
+	 */
+	@Override
+	@GET
+	@Path("fapi/v1/ticker/24hr")
+	List<BinanceTicker24h> ticker24h() throws IOException, BinanceException;
+	
+	/**
+	 * 24 hour price change statistics.
+	 *
+	 * @param symbol
+	 * @return
+	 * @throws IOException
+	 * @throws BinanceException
+	 */
+	@Override
+	@GET
+	@Path("fapi/v1/ticker/24hr")
+	BinanceTicker24h ticker24h(@QueryParam("symbol") String symbol)
+			throws IOException, BinanceException;
+
+	/**
+	 * Latest price for a symbol.
+	 *
+	 * @return
+	 * @throws IOException
+	 * @throws BinanceException
+	 */
+	@Override
+	@GET
+	@Path("fapi/v1/ticker/price")
+	BinancePrice tickerPrice(@QueryParam("symbol") String symbol)
+	    throws IOException, BinanceException;
+
+	/**
+	 * Latest price for all symbols.
+	 *
+	 * @return
+	 * @throws IOException
+	 * @throws BinanceException
+	 */
+	@Override
+	@GET
+	@Path("fapi/v1/ticker/price")
+	List<BinancePrice> tickerAllPrices() throws IOException, BinanceException;
+
+	/**
+	 * Best price/qty on the order book for all symbols.
+	 *
+	 * @return
+	 * @throws IOException
+	 * @throws BinanceException
+	 */
+	@Override
+	@GET
+	@Path("fapi/v1/ticker/bookTicker")
+	List<BinancePriceQuantity> tickerAllBookTickers() throws IOException, BinanceException;
+	
+	
+	
+	// -------------------- Extend BinanceAuthenticated API ---------------------------------------
+	// Override mutual methods and update to Futures Url
+	
     /**
      * Get current account information.
      *
@@ -36,15 +232,15 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
      * @throws IOException
      * @throws BinanceException
      */
-    BinanceFuturesAccountInformation futuresAccount(
+	@GET
+    @Path("fapi/v2/account")
+	BinanceFuturesAccountInformation futuresAccount(
             @QueryParam("recvWindow") Long recvWindow,
             @QueryParam("timestamp") SynchronizedValueFactory<Long> timestamp,
             @HeaderParam(X_MBX_APIKEY) String apiKey,
             @QueryParam(SIGNATURE) ParamsDigest signature)
             throws IOException, BinanceException;
 
-    @GET
-    @Path("fapi/v1/commissionRate")
     /**
      * @param symbol
      * @param recvWindow optional
@@ -55,6 +251,8 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
      * @throws IOException
      * @throws BinanceException
      */
+	@GET
+    @Path("fapi/v1/commissionRate")
     BinanceUserCommissionRate userCommissionRate(
             @QueryParam("symbol") String symbol,
             @QueryParam("recvWindow") Long recvWindow,
@@ -63,8 +261,6 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
             @QueryParam(SIGNATURE) ParamsDigest signature)
             throws IOException, BinanceException;
 
-    @GET
-    @Path("fapi/v1/openOrders")
     /**
      * Get open orders on a symbol.
      *
@@ -75,6 +271,8 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
      * @throws IOException
      * @throws BinanceException
      */
+	@GET
+    @Path("fapi/v1/openOrders")
     List<BinanceFuturesOrder> futuresOpenOrders(
             @QueryParam("symbol") String symbol,
             @QueryParam("recvWindow") Long recvWindow,
@@ -83,8 +281,6 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
             @QueryParam(SIGNATURE) ParamsDigest signature)
             throws IOException, BinanceException;
 
-    @GET
-    @Path("fapi/v1/allOrders")
     /**
      * Get all account orders; active, canceled, or filled.
      * These orders will not be found:
@@ -104,6 +300,8 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
      * @throws IOException
      * @throws BinanceException
      */
+	@GET
+    @Path("fapi/v1/allOrders")
     List<BinanceFuturesOrder> futuresAllOrders(
             @QueryParam("symbol") String symbol,
             @QueryParam("orderId") Long orderId,
@@ -116,10 +314,6 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
             @QueryParam(SIGNATURE) ParamsDigest signature)
             throws IOException, BinanceException;
 
-
-    @Override
-    @DELETE
-    @Path("fapi/v1/order")
     /**
      * Cancel an active order.
      *
@@ -136,6 +330,9 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
      * @throws IOException
      * @throws BinanceException
      */
+	@Override
+    @DELETE
+    @Path("fapi/v1/order")
     BinanceCancelledOrder cancelOrder(
             @QueryParam("symbol") String symbol,
             @QueryParam("orderId") Long orderId,
@@ -147,8 +344,6 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
             @QueryParam(SIGNATURE) ParamsDigest signature)
             throws IOException, BinanceException;
 
-    @POST
-    @Path("fapi/v1/order")
     /**
      * @param symbol
      * @param side
@@ -174,6 +369,8 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
      * @throws IOException
      * @throws BinanceException
      */
+	@POST
+    @Path("fapi/v1/order")
     BinanceFuturesOrder newFuturesOrder(
             @FormParam("symbol") String symbol,
             @FormParam("side") OrderSide side,
@@ -197,8 +394,6 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
             @QueryParam(SIGNATURE) ParamsDigest signature)
             throws IOException, BinanceException;
 
-    @GET
-    @Path("fapi/v1/order")
     /**
      * Check an order's status.<br>
      * Either orderId or origClientOrderId must be sent.
@@ -214,6 +409,8 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
      * @throws IOException
      * @throws BinanceException
      */
+	@GET
+    @Path("fapi/v1/order")
     BinanceFuturesOrder futuresOrderStatus(
             @QueryParam("symbol") String symbol,
             @QueryParam("orderId") Long orderId,
@@ -224,8 +421,6 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
             @QueryParam(SIGNATURE) ParamsDigest signature)
             throws IOException, BinanceException;
 
-    @GET
-    @Path("fapi/v1/userTrades")
     /**
      * Get trades for a specific account and symbol.
      *
@@ -242,6 +437,8 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
      * @throws IOException
      * @throws BinanceException
      */
+	@GET
+    @Path("fapi/v1/userTrades")
     List<BinanceFuturesTrade> myFuturesTrades(
             @QueryParam("symbol") String symbol,
             @QueryParam("limit") Integer limit,
@@ -254,83 +451,14 @@ public interface BinanceFuturesAuthenticated extends BinanceAuthenticated {
             @QueryParam(SIGNATURE) ParamsDigest signature)
             throws IOException, BinanceException;
 
-    @Override
-    @GET
-    @Path("fapi/v1/depth")
-    /**
-     * @param symbol
-     * @param limit optional, default 100 max 5000. Valid limits: [5, 10, 20, 50, 100, 500, 1000,
-     *     5000]
-     * @return
-     * @throws IOException
-     * @throws BinanceException
-     */
-    BinanceOrderbook depth(@QueryParam("symbol") String symbol, @QueryParam("limit") Integer limit)
-            throws IOException, BinanceException;
-
-    @Override
-    @GET
-    @Path("fapi/v1/ticker/24hr")
-    /**
-     * 24 hour price change statistics for all symbols. - bee carreful this api call have a big
-     * weight, only about 4 call per minut can be without ban.
-     *
-     * @return
-     * @throws IOException
-     * @throws BinanceException
-     */
-    List<BinanceTicker24h> ticker24h() throws IOException, BinanceException;
-
-    @Override
-    @GET
-    @Path("fapi/v1/ticker/24hr")
-    /**
-     * 24 hour price change statistics.
-     *
-     * @param symbol
-     * @return
-     * @throws IOException
-     * @throws BinanceException
-     */
-    BinanceTicker24h ticker24h(@QueryParam("symbol") String symbol)
-            throws IOException, BinanceException;
-
-    @Override
-    @GET
-    @Path("fapi/v1/aggTrades")
-    /**
-     * Get compressed, aggregate trades. Trades that fill at the time, from the same order, with the
-     * same price will have the quantity aggregated.<br>
-     * If both startTime and endTime are sent, limit should not be sent AND the distance between
-     * startTime and endTime must be less than 24 hours.<br>
-     * If frondId, startTime, and endTime are not sent, the most recent aggregate trades will be
-     * returned.
-     *
-     * @param symbol
-     * @param fromId optional, ID to get aggregate trades from INCLUSIVE.
-     * @param startTime optional, Timestamp in ms to get aggregate trades from INCLUSIVE.
-     * @param endTime optional, Timestamp in ms to get aggregate trades until INCLUSIVE.
-     * @param limit optional, Default 500; max 1000.
-     * @return
-     * @throws IOException
-     * @throws BinanceException
-     */
-    List<BinanceAggTrades> aggTrades(
-            @QueryParam("symbol") String symbol,
-            @QueryParam("fromId") Long fromId,
-            @QueryParam("startTime") Long startTime,
-            @QueryParam("endTime") Long endTime,
-            @QueryParam("limit") Integer limit)
-            throws IOException, BinanceException;
-
-    @GET
-    @Path("fapi/v1/exchangeInfo")
     /**
      * Current exchange trading rules and symbol information.
      *
      * @return
      * @throws IOException
      */
+	@GET
+    @Path("fapi/v1/exchangeInfo")
     BinanceFuturesExchangeInfo futuresExchangeInfo() throws IOException;
 
     /**
